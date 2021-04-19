d83eebb419ceb37f6ed1f93cb5dc82eae4b88204
Sergaav <sergaav@gmail.com>
2021-04-20 00:30:59 +0300

    Commit Part1

--- src/main/java/com/epam/rd/java/basic/practice6/part1/Part1.java
+++ src/main/java/com/epam/rd/java/basic/practice6/part1/Part1.java
@@ -1,8 +1,24 @@
 package com.epam.rd.java.basic.practice6.part1;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+
 public class Part1 {
 	
 	public static void main(String[] args) {
+		InputStream temp = System.in;
+		String data = "asd 43 asdf asd 43\n434 asdf\nkasdf asdf stop asdf\nstop\n";
+		InputStream is = new ByteArrayInputStream(data.getBytes());
+		System.setIn(is);
+		try {
+			WordContainer.main(args);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.setIn(temp);
+
 		
 	}
 
--- src/main/java/com/epam/rd/java/basic/practice6/part1/Word.java
+++ src/main/java/com/epam/rd/java/basic/practice6/part1/Word.java
@@ -2,13 +2,49 @@ package com.epam.rd.java.basic.practice6.part1;
 
 public class Word implements Comparable<Word> {
 
-	private String content;
-	
-	private int frequency;
+    private final String content;
+
+    private final int frequency;
+
+    public String getContent() {
+        return content;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+
+    public Word(String content, int frequency) {
+        this.content = content;
+        this.frequency = frequency;
+    }
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        return prime * result + ((content == null) ? 0 : content.hashCode());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        Word word = (Word) obj;
+
+        return frequency == word.getFrequency() && content.equals(word.content);
+    }
 
     @Override
     public int compareTo(Word o) {
-        return 0;
+        int result =  o.frequency - this.frequency;
+        if (result == 0) {
+            result = this.content.compareTo(o.content);
+        }
+        return result;
     }
 
 }
