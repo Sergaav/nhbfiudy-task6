e90410c88ae8eb49524371fbc65d139e57010e1e
Andrii Serha <sergaav@gmail.com>
2021-04-20 17:03:05 +0300

    Commit part2

--- src/main/java/com/epam/rd/java/basic/practice6/part2/Part2.java
+++ src/main/java/com/epam/rd/java/basic/practice6/part2/Part2.java
@@ -19,30 +19,29 @@ public class Part2 {
             list3.add(i);
         }
         long start = System.currentTimeMillis();
-        System.out.println(removeByIndex(list, 4));
-        System.out.println(System.currentTimeMillis() - start);
+        removeByIndex(list, 4);
+        System.out.println("ArrayList#Index: " + (System.currentTimeMillis() - start) + " ms");
         long start1 = System.currentTimeMillis();
-        System.out.println(removeByIndex(list1, 4));
-        System.out.println(System.currentTimeMillis() - start1);
+        removeByIndex(list1, 4);
+        System.out.println("LinkedList#Index: " + (System.currentTimeMillis() - start1) + " ms");
 
         long start2 = System.currentTimeMillis();
-        System.out.println(removeByIterator(list2, 4));
-        System.out.println(System.currentTimeMillis() - start2);
+        removeByIterator(list2, 4);
+        System.out.println("ArrayList#Iterator: " + (System.currentTimeMillis() - start2) + " ms");
         long start3 = System.currentTimeMillis();
-        System.out.println(removeByIterator(list3, 4));
-        System.out.println(System.currentTimeMillis() - start3);
+        removeByIterator(list3, 4);
+        System.out.println("LinkedList#Iterator: " + (System.currentTimeMillis() - start3) + " ms");
     }
 
-    public static long removeByIndex(final List<Integer> list, final int k) {
+    public static void removeByIndex(final List<Integer> list, final int k) {
         int pos = 0;
         while (list.size() != 1) {
             pos = (pos + k - 1) % list.size();
             list.remove(pos);
         }
-        return list.get(0);
     }
 
-    public static long removeByIterator(final List<Integer> list, int k) {
+    public static void removeByIterator(final List<Integer> list, int k) {
         Iterator<Integer> iterator;
         int pos = k;
         while (list.size() != 1) {
@@ -58,6 +57,5 @@ public class Part2 {
                 }
             }
         }
-        return list.get(0);
     }
 }
