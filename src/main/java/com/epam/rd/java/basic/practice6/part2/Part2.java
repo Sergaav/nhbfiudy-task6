32f2c0c78dd4ba005e940603f55d1835dc5e74a2
Andrii Serha <sergaav@gmail.com>
2021-04-20 16:52:58 +0300

    Commit part2

--- src/main/java/com/epam/rd/java/basic/practice6/part2/Part2.java
+++ src/main/java/com/epam/rd/java/basic/practice6/part2/Part2.java
@@ -10,44 +10,53 @@ public class Part2 {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
         List<Integer> list1 = new LinkedList<>();
+        List<Integer> list2 = new ArrayList<>();
+        List<Integer> list3 = new LinkedList<>();
         for (int i = 0; i < 10000; ++i) {
             list.add(i);
             list1.add(i);
+            list2.add(i);
+            list3.add(i);
         }
         long start = System.currentTimeMillis();
         System.out.println(removeByIndex(list, 4));
-        System.out.println(System.currentTimeMillis()-start);
+        System.out.println(System.currentTimeMillis() - start);
         long start1 = System.currentTimeMillis();
-        System.out.println(removeByIndex(list1,4));
-        System.out.println(System.currentTimeMillis()-start1);
+        System.out.println(removeByIndex(list1, 4));
+        System.out.println(System.currentTimeMillis() - start1);
 
         long start2 = System.currentTimeMillis();
-        System.out.println(removeByIterator(list, 4));
-        System.out.println(System.currentTimeMillis()-start2);
+        System.out.println(removeByIterator(list2, 4));
+        System.out.println(System.currentTimeMillis() - start2);
         long start3 = System.currentTimeMillis();
-        System.out.println(removeByIterator(list1,4));
-        System.out.println(System.currentTimeMillis()-start3);
+        System.out.println(removeByIterator(list3, 4));
+        System.out.println(System.currentTimeMillis() - start3);
     }
 
     public static long removeByIndex(final List<Integer> list, final int k) {
-        int pos =0;
-        while (list.size()!=1)
-        {
-            pos = (pos+k-1)%list.size();
+        int pos = 0;
+        while (list.size() != 1) {
+            pos = (pos + k - 1) % list.size();
             list.remove(pos);
         }
         return list.get(0);
     }
 
     public static long removeByIterator(final List<Integer> list, int k) {
-        int pos=0;
-        Iterator<Integer> iterator = list.iterator();
-        while (list.size() !=1){
-            pos = (pos+k-1)%list.size();
-            for (int i=0;i<pos;++i){
-                iterator.next();
+        Iterator<Integer> iterator;
+        int pos = k;
+        while (list.size() != 1) {
+            iterator = list.iterator();
+            while (iterator.hasNext()) {
+                if (pos > 0) {
+                    iterator.next();
+                    pos--;
+                }
+                if (pos == 0) {
+                    iterator.remove();
+                    pos = k;
+                }
             }
-            iterator.remove();
         }
         return list.get(0);
     }
