b880a5adf26b8a2702b93cb57d25bdbb638bb4d5
Andrii Serha <sergaav@gmail.com>
2021-04-21 12:34:03 +0300

    Commit part2

--- src/main/java/com/epam/rd/java/basic/practice6/part3/Parking.java
+++ src/main/java/com/epam/rd/java/basic/practice6/part3/Parking.java
@@ -1,21 +1,52 @@
 package com.epam.rd.java.basic.practice6.part3;
 
-public class Parking {
-    
-     public Parking(int capacity) {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Parking {
+    private final List<Integer> listParkingSlots = new ArrayList<>();
+
+    public Parking(int capacity) {
+        for (int i = 0; i < capacity; i++) {
+            listParkingSlots.add(0);
+        }
     }
-    
+
     public boolean arrive(int k) {
+        if (listParkingSlots.get(k) == 0) {
+            listParkingSlots.set(k, 1);
+            return true;
+        } else {
+            for (int i = k + 1; i < listParkingSlots.size(); i++) {
+                if (listParkingSlots.get(i) == 0) {
+                    listParkingSlots.set(i, 1);
+                    return true;
+                }
+            }
+            for (int i = 0; i < k; i++) {
+                if (listParkingSlots.get(i) == 0) {
+                    listParkingSlots.set(i, 1);
+                    return true;
+                }
+            }
+        }
         return false;
-        
     }
-    
+
     public boolean depart(int k) {
+        if (k < 0 || k >= listParkingSlots.size()) {
+            throw new IllegalArgumentException();
+        }
+        if (listParkingSlots.get(k) == 1) {
+            listParkingSlots.set(k, 0);
+            return true;
+        }
         return false;
     }
-    
+
     public void print() {
-        
+        for (Integer listParkingSlot : listParkingSlots) {
+            System.out.print(listParkingSlot);
+        }
     }
 }
--- src/main/java/com/epam/rd/java/basic/practice6/part3/Part3.java
+++ src/main/java/com/epam/rd/java/basic/practice6/part3/Part3.java
@@ -1,9 +1,42 @@
 package com.epam.rd.java.basic.practice6.part3;
 
 public class Part3 {
-    
+
     public static void main(String[] args) {
-        
+        Parking parking = new Parking(4);
+        System.out.print("Initial state of the parking: ");
+        parking.print();
+        System.out.println();
+        boolean condition;
+        condition = parking.arrive(2);
+        parking.print();
+        System.out.println(","+condition);
+
+        condition = parking.arrive(3);
+        parking.print();
+        System.out.println(","+condition);
+
+        condition = parking.arrive(2);
+        parking.print();
+        System.out.println(","+condition);
+
+        condition = parking.arrive(2);
+        parking.print();
+        System.out.println(","+condition);
+
+        condition = parking.arrive(2);
+        parking.print();
+        System.out.println(","+condition);
+
+        condition = parking.depart(1);
+        parking.print();
+        System.out.println(","+condition);
+
+        condition = parking.depart(1);
+        parking.print();
+        System.out.println(","+condition);
+
+
     }
 
 }
