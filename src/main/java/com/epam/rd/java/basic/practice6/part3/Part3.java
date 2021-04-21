99f3ccdf0dc8262456ad9e78352c65632a0077c7
Andrii Serha <sergaav@gmail.com>
2021-04-21 15:15:46 +0300

    Commit changes in part3

--- src/main/java/com/epam/rd/java/basic/practice6/part3/Part3.java
+++ src/main/java/com/epam/rd/java/basic/practice6/part3/Part3.java
@@ -1,42 +1,37 @@
 package com.epam.rd.java.basic.practice6.part3;
 
 public class Part3 {
+    private static boolean condition;
+    private static Parking parking;
 
     public static void main(String[] args) {
-        Parking parking = new Parking(4);
+        parking = new Parking(4);
         System.out.print("Initial state of the parking: ");
         parking.print();
         System.out.println();
-        boolean condition;
-        condition = parking.arrive(2);
-        parking.print();
-        System.out.println(","+condition);
 
+        condition = parking.arrive(2);
+        printOut();
         condition = parking.arrive(3);
-        parking.print();
-        System.out.println(","+condition);
-
+        printOut();
         condition = parking.arrive(2);
-        parking.print();
-        System.out.println(","+condition);
-
+        printOut();
         condition = parking.arrive(2);
-        parking.print();
-        System.out.println(","+condition);
-
+        printOut();
         condition = parking.arrive(2);
-        parking.print();
-        System.out.println(","+condition);
-
+        printOut();
         condition = parking.depart(1);
-        parking.print();
-        System.out.println(","+condition);
-
+        printOut();
         condition = parking.depart(1);
+        printOut();
+
+
+
+    }
+
+    public static void printOut (){
         parking.print();
         System.out.println(","+condition);
-
-
     }
 
 }
