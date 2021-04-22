32985ff8da350a806109f4761a44c7c6f509ddac
Andrii Serha <sergaav@gmail.com>
2021-04-22 15:31:45 +0300

    Commit part4 and tests

--- src/main/java/com/epam/rd/java/basic/practice6/part5/Part5.java
+++ src/main/java/com/epam/rd/java/basic/practice6/part5/Part5.java
@@ -5,13 +5,17 @@ public class Part5 {
     public static void main(String[] args) {
 
         Tree<Integer> tree = new Tree<>();
-        tree.add(5);
-        tree.add(8);
         tree.add(3);
-        tree.add(10);
+        tree.add(5);
+        tree.add(1);
         tree.add(2);
+        tree.add(0);
+        tree.add(4);
+        tree.add(6);
 
         tree.print();
+
+
 
         
     }
--- src/main/java/com/epam/rd/java/basic/practice6/part5/Tree.java
+++ src/main/java/com/epam/rd/java/basic/practice6/part5/Tree.java
@@ -1,8 +1,5 @@
 package com.epam.rd.java.basic.practice6.part5;
 
-import java.util.Stack;
-import java.util.TreeMap;
-
 public class Tree<E extends Comparable<E>> {
     private Node<E> rootNode;
 
@@ -11,33 +8,22 @@ public class Tree<E extends Comparable<E>> {
     }
 
     public boolean add(E element) {
-        Node<E> newNode = new Node<>();
-        newNode.setValue(element);
-        if (rootNode == null) {
-            rootNode = newNode;
-        } else {
-            Node<E> currentNode = rootNode;
-            Node<E> parentNode;
-            while (true) {
-                parentNode = currentNode;
-                if (element == currentNode.getValue()) {
-                    return false;
-                } else if (element.compareTo(currentNode.getValue()) < 0) {
-                    currentNode = currentNode.getLeftChild();
-                    if (currentNode == null) {
-                        parentNode.setLeftChild(newNode);
-                        return true;
-                    }
-                } else {
-                    currentNode = currentNode.getRightChild();
-                    if (currentNode == null) {
-                        parentNode.setRightChild(newNode);
-                        return true;
-                    }
-                }
-            }
+        rootNode = doInsert(rootNode, element);
+        return rootNode != null;
+    }
+
+    public Node<E> doInsert(Node<E> node, E x) {
+        if (node == null) {
+            node = new Node<>();
+            node.setValue(x);
+            return node;
         }
-        return false;
+        if (x.compareTo(node.getValue()) < 0) {
+            node.leftChild = doInsert(node.leftChild, x);
+        } else if (x.compareTo(node.getValue()) > 0) {
+            node.rightChild = doInsert(node.rightChild, x);
+        }
+        return node;
     }
 
 
@@ -48,142 +34,116 @@ public class Tree<E extends Comparable<E>> {
     }
 
     public boolean remove(E element) {
-        Node<E> currentNode = rootNode;
-        Node<E> parentNode = rootNode;
-        boolean isLeftChild = true;
-        while (currentNode.getValue() != element) {
-            parentNode = currentNode;
-            if (element.compareTo(currentNode.getValue()) < 0) {
-                isLeftChild = true;
-                currentNode = currentNode.getLeftChild();
-            } else {
-                isLeftChild = false;
-                currentNode = currentNode.getRightChild();
-            }
-            if (currentNode == null)
-                return false;
-        }
-
-        if (currentNode.getLeftChild() == null && currentNode.getRightChild() == null) {
-            if (currentNode == rootNode)
-                rootNode = null;
-            else if (isLeftChild)
-                parentNode.setLeftChild(null);
-            else
-                parentNode.setRightChild(null);
-        } else if (currentNode.getRightChild() == null) {
-            if (currentNode == rootNode)
-                rootNode = currentNode.getLeftChild();
-            else if (isLeftChild)
-                parentNode.setLeftChild(currentNode.getLeftChild());
-            else
-                parentNode.setRightChild(currentNode.getLeftChild());
-        } else if (currentNode.getLeftChild() == null) {
-            if (currentNode == rootNode)
-                rootNode = currentNode.getRightChild();
-            else if (isLeftChild)
-                parentNode.setLeftChild(currentNode.getRightChild());
-            else
-                parentNode.setRightChild(currentNode.getRightChild());
-        } else {
-            Node<E> heir = receiveHeir(currentNode);
-            if (currentNode == rootNode)
-                rootNode = heir;
-            else if (isLeftChild)
-                parentNode.setLeftChild(heir);
-            else
-                parentNode.setRightChild(heir);
-        }
-        return true;
+        Node<E> node = removeElement(rootNode, element);
+        return node != null;
     }
 
-
-    private Node<E> receiveHeir(Node<E> node) {
-        Node<E> parentNode = node;
-        Node<E> heirNode = node;
-        Node<E> currentNode = node.getRightChild();
-        while (currentNode != null) {
-            parentNode = heirNode;
-            heirNode = currentNode;
-            currentNode = currentNode.getLeftChild();
+    public Node<E> removeElement(Node<E> node, E el) {
+        if (node == null) {
+            return null;
         }
-
-        if (heirNode != node.getRightChild()) {
-            parentNode.setLeftChild(heirNode.getRightChild());
-            heirNode.setRightChild(node.getRightChild());
+        if (el.compareTo(node.getValue()) < 0) {
+            node.leftChild = removeElement(node.leftChild, el);
+            return node;
+        } else if (el.compareTo(node.getValue()) > 0) {
+            node.rightChild = removeElement(node.rightChild, el);
+            return node;
         }
-        return heirNode;
+        if (node.leftChild == null) {
+            return node.rightChild;
+        } else if (node.rightChild == null) {
+            return node.leftChild;
+        } else {
+            E minKey = find_min(node.rightChild).getValue();
+            node.setValue(minKey);
+            node.rightChild = removeElement(node.rightChild, minKey);
+            return node;
+        }
+    }
+
+    public Node<E> find_min(Node<E> node) {
+        if (node.leftChild != null) {
+            return find_min(node.leftChild);
+        } else {
+            return node;
+        }
     }
 
     public void print() {
-        Stack<Node<E>> globalStack = new Stack<>(); // общий стек для значений дерева
-        globalStack.push(rootNode);
-        int gaps = 32; // начальное значение расстояния между элементами
-        boolean isRowEmpty = false;
-        String separator = "-----------------------------------------------------------------";
-        System.out.println(separator);// черта для указания начала нового дерева
-        while (!isRowEmpty) {
-            Stack <Node<E>> localStack = new Stack<>(); // локальный стек для задания потомков элемента
-            isRowEmpty = true;
-
-            for (int j = 0; j < gaps; j++)
-                System.out.print(' ');
-            while (!globalStack.isEmpty()) { // покуда в общем стеке есть элементы
-                Node<E> temp = globalStack.pop(); // берем следующий, при этом удаляя его из стека
-                if (temp != null) {
-                    System.out.print(temp.getValue()); // выводим его значение в консоли
-                    localStack.push(temp.getLeftChild()); // соохраняем в локальный стек, наследники текущего элемента
-                    localStack.push(temp.getRightChild());
-                    if (temp.getLeftChild() != null ||
-                            temp.getRightChild() != null)
-                        isRowEmpty = false;
-                } else {
-                    System.out.print("__");// - если элемент пустой
-                    localStack.push(null);
-                    localStack.push(null);
-                }
-                for (int j = 0; j < gaps * 2 - 2; j++)
-                    System.out.print(' ');
-            }
-            System.out.println();
-            gaps /= 2;// при переходе на следующий уровень расстояние между элементами каждый раз уменьшается
-            while (!localStack.isEmpty())
-                globalStack.push(localStack.pop()); // перемещаем все элементы из локального стека в глобальный
+        if (rootNode.leftChild != null) {
+            printNode(rootNode.leftChild);
         }
-        System.out.println(separator);// подводим черту
+        System.out.println(rootNode.getValue());
+        if (rootNode.rightChild != null) {
+            printNode(rootNode.rightChild);
+        }
+
+    }
+
+    public void printNode(Node<E> node) {
+        int height = 0;
+        if (node.leftChild != null) {
+            printNode(node.leftChild);
+        } else if (node.rightChild != null) {
+            printNode(node.rightChild);
+        } else {
+            height = calcHeight(node);
+            for (int i = 0; i < height; i++) {
+                System.out.print("  ");
+            }
+            System.out.println(node.getValue());
+        }
+    }
+
+    public int calcHeight(Node<E> node) {
+        if (node != null) {
+            return node.height = Math.max(calcHeight(node.leftChild), calcHeight(node.rightChild)) + 1;
+        } else {
+            return -1;
+        }
     }
 
 
+    private static final class Node<E> {
+        E value;
+        Node<E> leftChild;
+        Node<E> rightChild;
+        Node<E> parent;
+        int height = 0;
 
-private static final class Node<E> {
-    E value;
-    Node<E> leftChild;
-    Node<E> rightChild;
+
+        public Node<E> getParent() {
+            return parent;
+        }
+
+        public void setParent(Node<E> parent) {
+            this.parent = parent;
+        }
 
 
-    public E getValue() {
-        return value;
+        public E getValue() {
+            return value;
+        }
+
+        public void setValue(E value) {
+            this.value = value;
+        }
+
+        public Node<E> getLeftChild() {
+            return leftChild;
+        }
+
+        public void setLeftChild(Node<E> leftChild) {
+            this.leftChild = leftChild;
+        }
+
+        public Node<E> getRightChild() {
+            return rightChild;
+        }
+
+        public void setRightChild(Node<E> rightChild) {
+            this.rightChild = rightChild;
+        }
     }
-
-    public void setValue(E value) {
-        this.value = value;
-    }
-
-    public Node<E> getLeftChild() {
-        return leftChild;
-    }
-
-    public void setLeftChild(Node<E> leftChild) {
-        this.leftChild = leftChild;
-    }
-
-    public Node<E> getRightChild() {
-        return rightChild;
-    }
-
-    public void setRightChild(Node<E> rightChild) {
-        this.rightChild = rightChild;
-    }
-}
 
 }
