8c047abe17c5556f7c0101fdc2beb3b95cde3994
Sergaav <sergaav@gmail.com>
2021-04-23 00:20:28 +0300

    Commit Part6

--- src/main/java/com/epam/rd/java/basic/practice6/Demo.java
+++ src/main/java/com/epam/rd/java/basic/practice6/Demo.java
@@ -4,16 +4,24 @@ import com.epam.rd.java.basic.practice6.part1.Part1;
 import com.epam.rd.java.basic.practice6.part2.Part2;
 import com.epam.rd.java.basic.practice6.part3.Part3;
 import com.epam.rd.java.basic.practice6.part4.Part4;
+import com.epam.rd.java.basic.practice6.part5.Part5;
+import com.epam.rd.java.basic.practice6.part6.Part6;
 
 public class Demo {
 
 	public static void main(String[] args) {
-		Part1.main(args);
-		Part2.main(args);
-		Part3.main(args);
-		Part4.main(args);
+//		Part1.main(args);
+//		Part2.main(args);
+//		Part3.main(args);
+//		Part4.main(args);
+//		Part5.main(args);
+//		System.out.println("~~~~~~~~~~~~Part6");
 
-	    
+		String fileName = "part6.txt";
+	//	Part6.main(new String[] {"--input", fileName, "--task","frequency"});
+//		Part6.main(new String[] {"--input", fileName, "--task","length"});
+		Part6.main(new String[] {"--input", fileName, "--task","duplicates"});
+
 	}
 
 }
--- src/main/java/com/epam/rd/java/basic/practice6/part6/Part62.java
+++ src/main/java/com/epam/rd/java/basic/practice6/part6/Part62.java
@@ -1,5 +1,13 @@
 package com.epam.rd.java.basic.practice6.part6;
 
+import java.util.*;
+
 public class Part62 {
 
+    public static void printThreeLongestWords(List<String> data) {
+        Map<String, Integer> map = new LinkedHashMap<>();
+        data.forEach((k) -> map.put(k, k.length()));
+        map.entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByValue())).limit(3)
+                .forEach((k) -> System.out.println(k.getKey() + " ==> " + k.getValue()));
+    }
 }
