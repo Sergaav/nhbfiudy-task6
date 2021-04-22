ea17513db4824499b0807363d6e30830761349b7
Andrii Serha <sergaav@gmail.com>
2021-04-22 16:42:20 +0300

    Commit part4 and tests

--- src/main/java/com/epam/rd/java/basic/practice6/part5/Tree.java
+++ src/main/java/com/epam/rd/java/basic/practice6/part5/Tree.java
@@ -1,14 +1,9 @@
 package com.epam.rd.java.basic.practice6.part5;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Tree<E extends Comparable<E>> {
     private Node<E> rootNode;
-
-    public Tree() {
-        rootNode = null;
-    }
 
     public boolean add(E element) {
         rootNode = doInsert(rootNode, element);
@@ -25,6 +20,8 @@ public class Tree<E extends Comparable<E>> {
             node.leftChild = doInsert(node.leftChild, x);
         } else if (x.compareTo(node.getValue()) > 0) {
             node.rightChild = doInsert(node.rightChild, x);
+        }else {
+            return null;
         }
         return node;
     }
@@ -57,16 +54,16 @@ public class Tree<E extends Comparable<E>> {
         } else if (node.rightChild == null) {
             return node.leftChild;
         } else {
-            E minKey = find_min(node.rightChild).getValue();
+            E minKey = findMin(node.rightChild).getValue();
             node.setValue(minKey);
             node.rightChild = removeElement(node.rightChild, minKey);
             return node;
         }
     }
 
-    public Node<E> find_min(Node<E> node) {
+    public Node<E> findMin(Node<E> node) {
         if (node.leftChild != null) {
-            return find_min(node.leftChild);
+            return findMin(node.leftChild);
         } else {
             return node;
         }
@@ -83,26 +80,18 @@ public class Tree<E extends Comparable<E>> {
         ArrayList<String> result = new ArrayList<>();
         if (node.leftChild != null) {
             Object [] temp = printNode(node.leftChild);
-            for (int i = 0; i < temp.length; i++) {
-                result.add("  "+ temp[i]);
+            for (Object o : temp) {
+                result.add("  " + o);
             }
         }
         result.add(node.getValue().toString());
         if (node.rightChild != null) {
             Object [] temp = printNode(node.rightChild);
-            for (int i = 0; i < temp.length; i++) {
-                result.add("  " + temp[i]);
+            for (Object o : temp) {
+                result.add("  " + o);
             }
         }
         return result.toArray();
-    }
-
-    public int calcHeight(Node<E> node) {
-        if (node != null) {
-            return node.height = Math.max(calcHeight(node.leftChild), calcHeight(node.rightChild)) + 1;
-        } else {
-            return -1;
-        }
     }
 
 
@@ -110,18 +99,6 @@ public class Tree<E extends Comparable<E>> {
         E value;
         Node<E> leftChild;
         Node<E> rightChild;
-        Node<E> parent;
-        int height = 0;
-
-
-        public Node<E> getParent() {
-            return parent;
-        }
-
-        public void setParent(Node<E> parent) {
-            this.parent = parent;
-        }
-
 
         public E getValue() {
             return value;
@@ -131,21 +108,6 @@ public class Tree<E extends Comparable<E>> {
             this.value = value;
         }
 
-        public Node<E> getLeftChild() {
-            return leftChild;
-        }
-
-        public void setLeftChild(Node<E> leftChild) {
-            this.leftChild = leftChild;
-        }
-
-        public Node<E> getRightChild() {
-            return rightChild;
-        }
-
-        public void setRightChild(Node<E> rightChild) {
-            this.rightChild = rightChild;
-        }
     }
 
 }
